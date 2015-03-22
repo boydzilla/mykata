75e7077b18d129657fc27fea1c61d52de760b4f3
boydzilla <boydzilla@users.noreply.github.com>
2015-03-22 13:22:22 -0400

    Adding Nunit to project and creating blank test case which does nothing but assert true to test nunit is working

--- mykata/BowlingGameUnitTest.cs
+++ mykata/BowlingGameUnitTest.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace mykata
 {
-    class BowlingGameUnitTest
+    [TestFixture]
+    public class BowlingGameUnitTest
     {
+        [SetUp]
+        public void Init()
+        {
 
+        }
+
+        [Test]
+        public void TestNothingAssertTrue()
+        {
+            Assert.IsTrue(true);
+        }
     }
 }
